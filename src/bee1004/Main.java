21604c98a1a7f4032fbaf62bc2cbfc9b0a38f4d1
unknown <gustavocamargodasilva@hotmail.com>
2024-01-05 20:29:49 -0300

    accepted 1004 - Produto Simples

--- src/bee1004/Main.java
+++ src/bee1004/Main.java
@@ -1,10 +1,25 @@
 package bee1004;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		Integer primeiraEntrada,segundaEntrada,resultado;
+		
+		System.out.println("Digite a entrada 1: ");
+		primeiraEntrada = sc.nextInt();
+		System.out.println("Digite a entrada 2:");
+		segundaEntrada = sc.nextInt();
+		
+		resultado = primeiraEntrada * segundaEntrada;
+		
+		System.out.println("PROD = " + resultado);
+		
+		
+		sc.close();
 	}
 
 }
